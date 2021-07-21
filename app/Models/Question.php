725c653aef3cea107ccd664793b7ef83058cd6e7
Rishni <ashameemeduma@gmail.com>
2021-07-21 21:10:45 +0530

    Genarate question models via artisan model genarator

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -19,6 +19,6 @@ class Question extends Model
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value)
+        $this->attributes['slug'] = Str::slug($value);
     }
 }
