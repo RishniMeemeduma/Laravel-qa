21670c8ea88f30caf1ba1c02ec77b346bef0ba0a
Rishni <ashameemeduma@gmail.com>
2021-07-25 11:23:23 +0530

    genaration answerss using faker

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -26,4 +26,15 @@ class Answer extends Model
     {
         return \Parsedown::instance()->text($this->body);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($answer){
+           $answer->question->increment('answers_count');
+           $answer->question->save();
+        });
+
+    }
 }
---
+++ database/factories/AnswerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Answer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AnswerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Answer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'body'=> $this->faker->paragraph(rand(3,7),true),
+            'user_id'=> \App\Models\User::pluck('id')->random(),
+            'votes_count' =>rand(0,5),
+        ];
+    }
+}
