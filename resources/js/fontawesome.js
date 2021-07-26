7efeeea5a100dc5d716484bab97ed8c44429802d
Rishni <ashameemeduma@gmail.com>
2021-07-26 19:40:38 +0530

    Add votes control to a question and anwers

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,8 @@
+import { library, icon,dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas,faTwitter)
+
+dom.watch()
+
