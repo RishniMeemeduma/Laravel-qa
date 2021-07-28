08ce9cf834e0a5f56854bbf8e7b0f8910d915e94
Rishni <ashameemeduma@gmail.com>
2021-07-28 20:05:57 +0530

    save answers

---
+++ app/Http/Controllers/AnswersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Answer;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class AnswersController extends Controller
+{
+    
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Question $question,Request $request)
+    {
+        
+       
+       $question->answers()->create($request->validate([
+        'body'=>'required'
+    ]) + ['body'=>$request->body,'user_id'=>\Auth::id()]);
+
+       return back()->with('success','successfully saved !!');
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Answer $answer)
+    {
+        //
+    }
+}
